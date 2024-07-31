b645d562dda832be6a95b730afccd52f4135155c
Neil <neil@mail.tca.org.tw>
2024-07-31 08:39:35 +0800

    OKOK072902

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_service/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,9 +17,22 @@ export class AppComponent implements OnInit {//組件實作OnInit介面是Angula
   //實作並非繼承 而是須實現該介面的方法
   //初始化組件時會呼叫ngOnInit()方法
   http = inject(HttpClient);//類似C#欄位同樣能使用訪問修飾 並在該類別都能使用 但get set需自行定義
-  title = 'client';
+  private accountService = inject(AccountService);
+  title = 'DatingApp';
   users: any;//等價於JS動態型別可隨意更換型別 如果users:unknown 賦值後必須明確給予型別才可使用否則報錯
   ngOnInit(): void {//初始化時呼叫該方法並且介面規定無傳回值
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers(){
     this.http.get('https://localhost:7016/api/users').subscribe({//get使用Observable物件封裝的請求能力,並且response會被Observable接收
       //但Observable物件是懶加載 必須訂閱subscribe才會啟動執行 HttpClient返回的是冷Observable 不會長時間存在 
       //但WebSocket、事件流或持續性的計時器 長時間存在或持續發送數據 需要手動取消訂閱來避免內存洩漏
